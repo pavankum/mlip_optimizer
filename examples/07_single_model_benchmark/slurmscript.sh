b5e9ed04f704be75feb2ed890fdbde417c4abf29
Pavan Behara <pavan.behara@openforcefield.org>
2026-03-11 11:49:52 -0700

    add some more input jsons, slurmscript

---
+++ examples/07_single_model_benchmark/slurmscript.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+#SBATCH -J mlip
+#SBATCH -p standard
+#SBATCH -t 48:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks=40
+#SBATCH --cpus-per-task=1
+#SBATCH --mem=180gb
+#SBATCH --account dmobley_lab
+#SBATCH --export ALL
+#SBATCH --requeue
+# Set the output and error output paths.
+#SBATCH -o  slurm-%J.out
+#SBATCH -e  slurm-%J.err
+#
+
+source ~/.bashrc
+start=`date +%s`
+conda activate /dfs9/dmobley-lab/pbehara/conda-env/mlip_optimizer
+
+python optimize_single_model.py ./inputs/CONFIG_JSON
+
+end=`date +%s`
+runtime=$((end-start))
+printf -v formatted_runtime "%02d:%02d:%02d" $((runtime/3600)) $(( (runtime%3600)/60 )) $((runtime%60))
+echo "Time taken for sorting: $formatted_runtime"
+echo "done"
+
---
+++ examples/07_single_model_benchmark/submit.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for file in optimize_aceff.json optimize_aimnet2.json optimize_mace.json optimize_sage.json;do sed -i "s/CONFIG_JSON/${file}/g" slurmscript.sh;sbatch slurmscript.sh;sed -i "s/${file}/CONFIG_JSON/g" slurmscript.sh;done
+
+for file in optimize_mace_omol.json optimize_fennix_bio1_ion.json optimize_mace_off24_medium.json;do sed -i "s/CONFIG_JSON/${file}/g" slurmscript.sh;sbatch slurmscript.sh;sed -i "s/${file}/CONFIG_JSON/g" slurmscript.sh;done
+
