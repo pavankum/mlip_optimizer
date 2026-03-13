5bf58fda1e4bd85c5dc12f1216ff709675f4f2ca
Pavan Behara <pavan.behara@openforcefield.org>
2026-03-12 23:44:15 -0700

    add fennol, aimnet, hugginface_hub to conda yaml

--- examples/07_single_model_benchmark/submit.sh
+++ examples/07_single_model_benchmark/submit.sh
@@ -4,3 +4,4 @@ for file in optimize_aceff.json optimize_aimnet2.json optimize_mace.json optimiz
 
 for file in optimize_mace_omol.json optimize_fennix_bio1_ion.json optimize_mace_off24_medium.json;do sed -i "s/CONFIG_JSON/${file}/g" slurmscript.sh;sbatch slurmscript.sh;sed -i "s/${file}/CONFIG_JSON/g" slurmscript.sh;done
 
+for file in optimize_fennix_bio1_ion.json;do sed -i "s/CONFIG_JSON/${file}/g" slurmscript.sh;sbatch slurmscript.sh;sed -i "s/${file}/CONFIG_JSON/g" slurmscript.sh;done
